21bceb96fd1c333f32c08868546637506b0b6793
7R1XY <theo.broden@gmail.com>
2023-10-04 10:58:31 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            string h = Console.ReadLine();
+
+            int timmar = int.Parse(h);
+
+            int sum = timmar * 80;
+
+            if (sum > 950)
+            {
+                Console.WriteLine("Det kostar 950 kr");
+            }
+
+            else
+            {
+                Console.WriteLine("Det kostar "+ sum + " kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
